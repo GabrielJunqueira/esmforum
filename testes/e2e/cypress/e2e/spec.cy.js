b362530bc877c904884dfaa44ed68c5e4d4957a5
Gabriel Junqueira <64487574+GabrielJunqueira@users.noreply.github.com>
2023-10-29 16:38:35 -0300

    Update spec.cy.js

--- testes/e2e/cypress/e2e/spec.cy.js
+++ testes/e2e/cypress/e2e/spec.cy.js
@@ -1,21 +1,14 @@
-describe('Teste de cadastro de resposta', () => {
-  it('Cadastra uma resposta e verifica se ela é listada', () => {
-    cy.visit('http://localhost:3000');
+describe('Primeiro teste end-to-end', () => {
+  it('Cadastra uma pergunta e verifica se ela é listada', () => {
+    cy.visit('localhost:3000');
+    
+    cy.get('#textarea-pergunta').type('3+3');
+    cy.get('#btn-pergunta').click();
+    cy.get('#tabela-perguntas').contains('3+3');
 
-    cy.get('#btn-perguntas').click();
-
-    cy.get('#tabela-perguntas').contains('3+3').find('.btn-responder').click();
-
-    // Preenche o formulário de resposta com o texto desejado
-    cy.get('#textarea-resposta').type('A resposta é 6');
-
-    cy.get('#btn-enviar-resposta').click();
-
-    // Verificar se a resposta foi cadastrada com sucesso
-    cy.get('#tabela-respostas').contains('A resposta é 6');
+    //cy.visit('localhost:3000/resposta/1');
   });
 });
-
 
 
 
